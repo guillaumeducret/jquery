0395bbe290d22485fae7bed15cf97cc374ae7eda
Guillaume Ducret <guillaumeducret@yahoo.fr>
2012-09-06 19:19:36 +0200

    Proxy the native xhr object. When used with Rhino, the native xhr attributes are native types. This is in issue when jquery do a type check.

--- src/ajax.nodom.js
+++ src/ajax.nodom.js
@@ -2,7 +2,24 @@
 
 function createStandardXHR() {
 	try {
-		return nodom.XMLHttpRequest();
+		var xhr = nodom.XMLHttpRequest();
+		var xhrProxy = {
+			open : function(method, url, async) {
+				this.requestURl = url;
+				return xhr.open(method, url, async);
+			},
+			send : function(query) {
+				var result = xhr.send(query);
+				this.responseText = xhr.responseText + "";
+				this.status = xhr.status;
+				this.readyState = xhr.readyState;
+				return result;
+			},
+			getAllResponseHeaders : function() {
+				return xhr.getAllResponseHeaders();
+			}
+		};
+		return xhrProxy;
 	} catch (e) {
 	}
 }
